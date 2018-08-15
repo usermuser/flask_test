436fe13861b3c6d4427d9fde2bb6b97467f1e8d1
usermuser <lwrdrstv@gmail.com>
2018-08-15 23:56:25 +0500

    added more routes from quickstart guide

--- hello.py
+++ hello.py
@@ -1,6 +1,36 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
+def index():
+    return 'Index Page'
+
+
+@app.route('/hello')
 def hello_world():
-	return 'Hello World!'
+    return 'Hello World!'
+
+
+@app.route('/username/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % post_id
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return 'Subpath %s' % subpath
+
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
