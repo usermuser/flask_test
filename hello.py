f18a384cbd39db8a99775299e2047f8da64f2d66
usermuser <lwrdrstv@gmail.com>
2018-08-16 14:23:54 +0500

    added url_for and app.test_request_context

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 
@@ -12,14 +12,20 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/username/<username>')
-def show_user_profile(username):
-    return 'User %s' % username
+@app.route('/login')
+def login():
+    return 'login'
+
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(username)
 
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
-    return 'Post %d' % post_id
+    if isinstance(post_id, int):
+        return 'Post %d' % post_id
 
 
 @app.route('/path/<path:subpath>')
@@ -34,3 +40,15 @@ def projects():
 @app.route('/about')
 def about():
     return 'The about page'
+
+@app.route('/text/<string:my_shiny_string>')
+def my_string(my_shiny_string):
+    return 'String %s ' % my_shiny_string
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
+
